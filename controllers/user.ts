ee22ce590fb40c30add12e02e0c74c44c10c5798
Shantanu Wable <shantanuwable2003@gmail.com>
2023-10-02 00:03:51 +0530

    to be updated

--- controllers/user.ts
+++ controllers/user.ts
@@ -1,79 +1,80 @@
 import * as dotenv from "dotenv";
 dotenv.config(); // Load environment variables from .env file
 import { Request, Response } from "express";
+import { Error, Document } from "mongoose";
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 
-// import { queryDatabase } from "../database/connection";
+import { user } from "../models/user";
+
 import { Role } from "../enums/role";
+
 import { Token } from "../interfaces/token";
 
 // User Functions
 
 export const createUser = async (req: Request, res: Response) => {
-//     try {
-//         const username = req.params.username;
+    try {
+        // Check if username exists
+        const username = req.params.username;
+        const retrievedUser = await user.findOne({ username }).exec();
+        if (retrievedUser) {
+            return res.status(400).json({
+                status: false,
+                message: "User already exists",
+            });
+        }
 
-//         // Check if username already exists
-//         let retrieveUserQuery = `SELECT *
-//             FROM users
-//             WHERE username = '${username}'`;
-//         let retrieveUser = await queryDatabase(retrieveUserQuery);
-//         if (retrieveUser.length !== 0) {
-//             return res.status(400).json({
-//                 status: false,
-//                 message: "Sorry the username has been taken",
-//             });
-//         }
+        const { first_name, last_name, phone, email, password } = req.body;
 
-//         const { first_name, last_name, phone, email, password } = req.body;
+        // Check if the email is associated with a different username
+        const retrievedUser2 = await user.findOne({username, email}).exec();
+        if (retrievedUser2) {
+            return res.status(400).json({
+                status: false,
+                message: "Email already exists",
+            });
+        }
 
-//         // Check if the email is associated with a different username
-//         retrieveUserQuery = `SELECT *
-//             FROM users
-//             WHERE email = '${email}'`;
-//         let retrieveUser2 = await queryDatabase(retrieveUserQuery);
-//         if (retrieveUser2.length !== 0) {
-//             return res.status(400).json({
-//                 status: false,
-//                 message:
-//                     "Sorry, the email is associated with a different username",
-//             });
-//         }
+        // Hashing the password using bcrypt
+        const salt = await bcrypt.genSalt(10);
+        const securedPassword = await bcrypt.hash(password, salt);
 
-//         // Hashing the password using bcrypt
-//         const salt = await bcrypt.genSalt(10);
-//         const securedPassword = await bcrypt.hash(password, salt);
+        // Creating a new user
+        const newUser = new user({
+            username,
+            first_name,
+            last_name,
+            phone,
+            email,
+            password: securedPassword,
+        });
 
-//         const createUserQuery = `SELECT InsertUserAndGetID ('${username}' , '${first_name}', '${last_name}', '${email}', '${securedPassword}', '${Role.user}', '${phone}');`;
+        const savedUser = await newUser.save();
 
-//         // const createUser = await queryDatabase(createUserQuery);
-//         const createUserQueryArray = await queryDatabase(createUserQuery);
-//         const user_id = Object.values(createUserQueryArray[0])[0];
+        const payloadData = {
+            id: savedUser.user_id,
+            username,
+            email,
+            role: Role.user,
+        };
 
-//         const payloadData = {
-//             id: user_id,
-//             username,
-//             email,
-//             role: Role.user,
-//         };
+        // Creating a JWT token
+        const authToken = jwt.sign(payloadData, process.env.JWT_SECRET!);
 
-//         // Creating a JWT token
-//         const authToken = jwt.sign(payloadData, process.env.JWT_SECRET!);
-
-//         return res.status(201).json({
-//             status: true,
-//             message: "User created successfully",
-//             user_id,
-//             authToken,
-//         });
-//     } catch (error) {
-//         return res.status(500).json({
-//             status: false,
-//             message: "Some error occured while creating a user",
-//             error: error,
-//         });
-//     }
+        return res.status(201).json({
+            status: true,
+            message: "User created successfully",
+            id: savedUser.user_id,
+            authToken,
+        });
+    } catch (error) {
+        return res.status(500).json({
+            status: false,
+            message: "Some error occured while creating a user",
+            error: error,
+        });
+    }
 };
 
 export const loginUser = async (req: Request, res: Response) => {
