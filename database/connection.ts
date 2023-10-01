138a9f0fbe51d2a01c165a7a26fc4592acaceee0
Shantanu Wable <shantanuwable2003@gmail.com>
2023-10-01 23:15:31 +0530

    Changed connection method

--- database/connection.ts
+++ database/connection.ts
@@ -1,48 +1,9 @@
-// Load environment variables
-require("dotenv").config();
-import mysql, { PoolOptions, RowDataPacket } from 'mysql2';
+const mongoose = require("mongoose");
 
-// Database configuration
-const dbConfig: mysql.PoolOptions = {
-    host: process.env.DATABASE_HOST,
-    user: process.env.DATABASE_USER,
-    port: 3306,
-    password: process.env.DATABASE_PASSWORD,
-    database: process.env.DATABASE_DATABASE,
+export const connectDB = (url: String) => {
+  mongoose.connect(url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    dbName: "PageTalk"
+  });
 };
-
-// Function to perform a query on the database
-export async function queryDatabase(query: string): Promise<RowDataPacket[]> {
-    const pool = mysql.createPool({
-        ...dbConfig,
-        waitForConnections: true,
-        connectionLimit: 10,
-        maxIdle: 10,
-        idleTimeout: 60000,
-        queueLimit: 0,
-        enableKeepAlive: true,
-        keepAliveInitialDelay: 0
-    });
-    return new Promise((resolve, reject) => {
-        pool.getConnection((err, connection) => {
-            if(err) {
-                reject(err);
-                return;
-            }
-
-            connection.query(
-                query,
-                (error, results: RowDataPacket[]) => {
-                    connection.release();
-                    if(error) {
-                        reject(error);
-                    } else {
-                        resolve(results);
-                        // console.log(rows)
-                        // console.log(results)
-                    }
-                }
-            )
-        }) 
-    })
-}
