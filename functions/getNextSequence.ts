2c6c9bbfcb8db27d808be8fae1f7e340033c7a59
Shantanu <shantanuwable2003@gmail.com>
2023-10-02 10:54:28 +0530

    Updated sequence function

--- functions/getNextSequence.ts
+++ functions/getNextSequence.ts
@@ -3,10 +3,10 @@ import { sequence } from "../models/sequence";
 import { sequenceInterface } from "../interfaces/sequence";
 
 export async function getNextSequenceValue(collectionName: string): Promise<number> {
-  const filter = { _id: collectionName }; // Use collectionName as the _id
+  const filter = { collectionName }; // Use collectionName as the _id
   const update = { $inc: { sequenceValue: 1 } };
   const options = { new: true, upsert: true };
 
-  const retrievedSequence = await sequence.findByIdAndUpdate(filter, update, options);
+  const retrievedSequence = await sequence.findOneAndUpdate(filter, update, options);
   return retrievedSequence?.sequenceValue ?? 1;
 }
