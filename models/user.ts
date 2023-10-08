deb083b5fdeef0947d8dd8fe593dee3e6776d000
Shantanu <shantanuwable2003@gmail.com>
2023-10-08 16:51:14 +0530

    Removed user_id and pre-save hook

--- models/user.ts
+++ models/user.ts
@@ -1,9 +1,7 @@
-import { Error, Schema, model } from "mongoose";
+import { Schema, model } from "mongoose";
 import { userInterface } from "../interfaces/user";
-import { getNextSequenceValue } from "../functions/getNextSequence";
 
 const userSchema = new Schema<userInterface>({
-    user_id: Number,
     username: { type: String, required: true, unique: true },
     first_name: { type: String, required: true },
     last_name: String,
@@ -17,20 +15,6 @@ const userSchema = new Schema<userInterface>({
         updated_at: { type: Date, default: Date.now },
     },
     last_login: { type: Date, default: Date.now },
-});
-
-userSchema.pre("save", async function (this: userInterface, next) {
-    if(this.isNew) {
-        try {
-            const seqValue = await getNextSequenceValue("user");
-            this.user_id = seqValue;
-            next();
-        } catch (error: any) {
-            next(error);
-        }
-    } else {
-        next();
-    }
 });
 
 export const user = model<userInterface>("User", userSchema);
