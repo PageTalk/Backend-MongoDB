b4cf284afc3a262fd6b5b606779e676512606430
Shantanu <shantanuwable2003@gmail.com>
2023-10-15 22:57:24 +0530

    not-found now serves static 404 page

--- middleware/not-found.ts
+++ middleware/not-found.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from "express";
+import path from "path";
 
 const notFound = (req: Request, res: Response) =>
-    res.status(404).send("Route does not exist");
+    res.status(404).sendFile(path.resolve("public/index.html"));
 
 module.exports = notFound;
